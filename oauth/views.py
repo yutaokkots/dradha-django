dfc284ecbf73b2c77419a8c0d6cb28f15eb28e36
yutaokkots <yuta.okkotsu@gmail.com>
2023-12-03 00:12:30 -0700

    Reformatted oauth.views for proper Python linting.

--- oauth/views.py
+++ oauth/views.py
@@ -1,27 +1,22 @@
-from django.shortcuts import render
-from rest_framework import status, request
+"""Class-based views for the oauth and oauth related uri endpoints."""
+import urllib
+import logging
+import random
+import json
+from datetime import datetime
+import os
+import string
+import requests
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.request import Request
 from django.shortcuts import redirect
 from django.core.cache import cache
 from django.http import HttpResponseServerError
-import requests
-import logging
-import os
-from urllib.parse import urlsplit, parse_qs
-import urllib.parse
-import random
-import string
-import json
-from datetime import datetime
-
-# test that I receive something at the callback route
-# code
-# state (user provided)
 
 GITHUB_URL = 'https://github.com/login/oauth/access_token' # os.environ['SECRET_GITHUB_TOKEN_URL'] #
 GITHUB_URL_USER = 'https://api.github.com/user'
+
 class GithubStateGenerator(APIView):
     """Class for generating and storing a state for Github OAuth Authorization"""
 
@@ -36,7 +31,7 @@ class GithubStateGenerator(APIView):
                 "timestamp":ts,
             }
             self.set_state(rand_state)
-            # cache.set(key, value, timeout=DEFAULT_TIMEOUT, version=None) # DEFAULT_TIMEOUT is in seconds, an int. 
+
             print(response)
             return Response(data=response, status=status.HTTP_201_CREATED)
         except ValueError as value_error:
@@ -44,41 +39,43 @@ class GithubStateGenerator(APIView):
             return Response(data={"error": error_message}, status=status.HTTP_400_BAD_REQUEST)
 
     def state_generator(self, length:int) -> str:
+        """Generates a random state of length, 'length:int'. """
         code = [random.choice(string.ascii_letters + string.digits) for _ in range(length)]
         return "".join(code)
-    
-    def timestamp(self):
+
+    def timestamp(self) -> str:
+        """Creates an ISO format timestamp"""
         return json.dumps(datetime.now().isoformat())
-    
+
     def set_state(self, state):
-        """Saves the generated state in cache with a time-out of 10 minutes (600 seconds)."""
+        """Saves the generated state in cache with a time-out of 10 minutes (600 seconds).
+
+        For caching in psql database:
+        cache.set(key, value, timeout=DEFAULT_TIMEOUT, version=None) 
+        # DEFAULT_TIMEOUT is in seconds, an int.
+        """
         key_state = state[:5]
-        cache.set(key_state, state, timeout=600) 
+        cache.set(key_state, state, timeout=600)
 
 class GithubOauthAPI(APIView):
     """ Class for the callback route once user submits login information 
     for Github OAuth2 authenticaion. 
     """
-    
+
     def get(self, request, *args, **kwargs):
-        try: 
+        """GET HTTP method for the callback URI for requesting an Auth token from Github."""
+        try:
             params = request.query_params
             params_state = params["state"]
             code = params["code"]
             if self.verify_state(params_state) and code:
                 url = GITHUB_URL
                 params = self.params_parser(code)
-                response = requests.post(url=url, params=params)
-
+                timeout_seconds = 10
+                response = requests.post(url=url, params=params, timeout=timeout_seconds)
                 response_data = str(response.content, encoding='utf-8')
-                # response_data = 'access_token=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&scope=&token_type=bearer'
-                # print(response_data)
-  
-                parameters = parse_qs(response_data)
-                # print(parameters)
-                # print(parameters["access_token"])
-                # print(parameters["access_token"][0])
-                
+                parameters = urllib.parse.parse_qs(response_data)
+
                 access_token = parameters["access_token"][0]
 
                 user = self.user_access(access_token)
@@ -93,7 +90,9 @@ class GithubOauthAPI(APIView):
         return redirect('http://localhost:3000')
 
     def verify_state(self, state:str):
-        """ Gets the state by its key (first 5 chars), and verifies the cached state is equal to the input state."""
+        """ Gets the state by its key (first 5 chars), 
+            and verifies the cached state is equal to the input state.
+        """
         value = cache.get(state[:5])
         print(value == state)
         return value == state
@@ -110,32 +109,13 @@ class GithubOauthAPI(APIView):
 
 
     def user_access(self, auth_token):
+        """ Accesses the user information from the Github server."""
         try:
             headers = {"Authorization": f"Bearer {auth_token}"}
-            response = requests.get(GITHUB_URL_USER, headers=headers)
+            timeout_seconds = 10
+            response = requests.get(GITHUB_URL_USER, headers=headers, timeout=timeout_seconds)
             response.raise_for_status()  # Raise HTTPError for bad responses
             return response.json()
         except requests.RequestException as e:
             logging.error(f"Error accessing token: {e}")
             return None
-
-
-
-
-
-    def post(self, request, *args, **kwargs):
-        print("POST!")
-        # try:
-        #     self.code = request.data["code"]
-        # except KeyError as e:
-        #     return Response({"error": f"Missing {e} parameter"}, status=status.HTTP_400_BAD_REQUEST)
-        # urlparams = self.post_github()
-        # #
-        # self.access_token_url = f"{GITHUB_URL}?{urlparams}"
-        # print(self.access_token_url)
-        # #logging.info(self.access_token_url)
-        # print(self.token_access())
-        # return Response({}, status=status.HTTP_201_CREATED)
-    
-
-
